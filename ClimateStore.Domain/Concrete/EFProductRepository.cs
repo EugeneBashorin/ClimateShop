c4c2bc8ece3b35e0af8935802a4719b48a73f443
EugeneBashorin <bashorinjeka@gmail.com>
2017-07-10 19:57:07 +0300

    add DB / ninject /  Entity

---
+++ ClimateStore.Domain/Concrete/EFProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClimateStore.Domain.Abstract;
+using ClimateStore.Domain.Entities;
+
+namespace ClimateStore.Domain.Concrete
+{
+   public class EFProductRepository: IProductRepository
+    {
+        private EFDbContext context = new EFDbContext();
+
+        public IQueryable<Product> Products
+        {
+            get { return context.Products; }
+        }
+    }
+}
+/*
+ класс хранилища реализует интерфейс IProductRepository и использует экземпляр EFDbContext, чтобы извлекать данные из базы с помощью Entity Framework
+*/
