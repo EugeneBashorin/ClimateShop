df9c18925019a15fb361c25193dab3e8c8fe4389
EugeneBashorin <bashorinjeka@gmail.com>
2017-07-24 14:27:14 +0300

    Added CRUD controller AdminController to Edit, Create and Del items. Added Unit Tests for CRUD's metods Added validation for server and client(jQeury libs)

---
+++ ClimateStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ClimateStore.Domain.Abstract;
+using System.Web.Mvc;
+using ClimateStore.Domain.Entities;
+
+namespace ClimateStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IProductRepository repository;
+
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index()
+        {
+            return View(repository.Products);
+        }
+
+        public ViewResult Edit(int productId)
+        {
+            Product product = repository.Products
+                .FirstOrDefault(p => p.ProductID == productId);
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)//механизм связывания провел валидацию представленных пользователем данных?
+            {
+                repository.SaveProduct(product);
+                TempData["message"] = string.Format("{0} has been saved", product.Name);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // there is something wrong with the data values
+                return View(product);
+            }
+
+        }
+
+        public ViewResult Create()
+        {
+            return View("Edit", new Product());
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int productID)
+        {
+            Product deletedProduct = repository.DeleteProduct(productID);
+            if(deletedProduct != null)
+            {
+                TempData["message"] = string.Format("{0} was deleted", deletedProduct.Name);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
